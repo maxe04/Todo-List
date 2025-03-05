8d47c2499e14e8107b8336d732090368d20530e9
Maximilian Günther <maxigunther9@gmail.com>
2025-03-05 21:26:30 +0100

    Final Polishing
    -Added New Functionality
    -Reworked Code and Comments

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,12 +1,17 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
@@ -14,13 +19,13 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.border.LineBorder;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-import javax.swing.text.Document;
 import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
@@ -28,12 +33,21 @@ import javax.swing.text.StyledDocument;
 public class MainWindow {
 
 	JFrame frame;
-	JPanel panel;
+	JPanel mainPanel;
+
 	JPanel titlePanel;
 	JLabel titleLabel;
-	JButton addTaskButton;
-	
-	File saveFile;
+
+	JButton newTaskButton;
+
+	JMenuBar menuBar;
+	JMenu menu;
+	JMenuItem saveMenuItem;
+	JMenuItem exitMenuItem;
+
+	enum STATE {
+		CHECKED, UNCHECKED
+	}
 
 	ArrayList<JPanel> tasks = new ArrayList<>();
 
@@ -43,92 +57,143 @@ public class MainWindow {
 
 	private void initialize() {
 
-		// Initialize JFrame
+		// Initialize Main Frame
 		frame = new JFrame();
 		frame.setSize(600, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setTitle("Todo List");
-
 		frame.setVisible(true);
 
+		// Initialize Title Panel
 		// Initialize Title Label
 		titlePanel = new JPanel();
 		titleLabel = new JLabel("Todo List");
+
 		titleLabel.setFont(new Font("Arial", Font.PLAIN, 40));
 
 		titlePanel.add(titleLabel);
 		frame.add(titlePanel, BorderLayout.NORTH);
 
 		// Initialize Main Panel
-		panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		frame.add(mainPanel, BorderLayout.CENTER);
 
-		// Add New Task Panel To List
-		tasks.add(newTask());
-		// Add Task Panel To Main Panel
-		panel.add(tasks.getFirst());
+		// Initialize New Task Button
+		newTaskButton = new JButton("New Task");
+		newTaskButton.setFocusable(false);
 
-		frame.add(panel, BorderLayout.CENTER);
-
-		// Initialize Add Task Button
-		addTaskButton = new JButton("Add Task");
-		addTaskButton.setFocusable(false);
-		addTaskButton.addActionListener(new ActionListener() {
+		// New Task Button Functionality
+		newTaskButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// Grey Out Old Task
-				if (!tasks.isEmpty()) {
-					tasks.getLast().getComponent(1).setEnabled(false);
-				}
 
-				// Add New Task Panel
-				tasks.add(newTask());
-				panel.add(tasks.getLast());
-				panel.revalidate();
-				panel.repaint();
-
+				// Add New Task
+				tasks.add(addTask("", STATE.UNCHECKED));
+				mainPanel.add(tasks.getLast());
+				mainPanel.revalidate();
+				mainPanel.repaint();
 			}
 		});
+		frame.add(newTaskButton, BorderLayout.SOUTH);
 
-		frame.add(addTaskButton, BorderLayout.SOUTH);
-
-		// Add Scroll Pane
-		JScrollPane scrollPane = new JScrollPane(panel);
+		// Initialize Scroll Pane
+		JScrollPane scrollPane = new JScrollPane(mainPanel);
 		scrollPane.setBorder(null);
 		frame.add(scrollPane);
 
+		// Initialize Menu
+
+		menuBar = new JMenuBar();
+		menu = new JMenu("Menu");
+
+		saveMenuItem = new JMenuItem("Save");
+		exitMenuItem = new JMenuItem("Exit");
+
+		menu.add(saveMenuItem);
+		menu.add(exitMenuItem);
+
+		menuBar.add(menu);
+		frame.setJMenuBar(menuBar);
+
+		saveMenuItem.addActionListener(e -> {
+			saveTasks();
+		});
+		exitMenuItem.addActionListener(e -> System.exit(0));
+
+		// Load Tasks
+		loadTasks();
+
 	}
 
-	// Task Panel Template
-	private JPanel newTask() {
+	// Task Template
+	private JPanel addTask(String task, STATE state) {
 		JPanel taskPanel = new JPanel();
 		JCheckBox checkbox = new JCheckBox();
-		JTextPane taskTextPane = new JTextPane();
+		JTextPane taskText = new JTextPane();
 		JButton removeButton = new JButton("X");
-		JButton editButton = new JButton("Edit");
+		JButton editButton;
+
+		// Disable Checkbox by default
+		checkbox.setEnabled(false);
+
+		// Enable Checkbox Only If Task Was Added
+		// Disable Task Text When Task Was Loaded In From File
+		if (task.isEmpty()) {
+			editButton = new JButton("Add");
+
+		} else {
+			editButton = new JButton("Edit");
+			checkbox.setEnabled(true);
+			taskText.setEnabled(false);
+		}
 
 		// Change Task Panel Size
 		taskPanel.setMaximumSize(new Dimension(600, 45));
 
-		// Set Checkbox Disabled As Default
-		if (taskTextPane.getText().isEmpty()) {
-			checkbox.setEnabled(false);
-		}
-		
+		// Customize Edit Button
+		editButton.setFocusable(false);
+		editButton.setPreferredSize(new Dimension(70, 30));
+
+		// Edit Button Functionality
+		editButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				if (editButton.getText().equals("Add")) {
+					taskText.setEnabled(false);
+					editButton.setText("Edit");
+
+					// Enable Checkbox Only If Task Was Added
+					checkbox.setEnabled(true);
+				} else if (editButton.getText().equals("Edit")) {
+					taskText.setEnabled(true);
+					editButton.setText("Done");
+				} else if (editButton.getText().equals("Done")) {
+					taskText.setEnabled(false);
+					editButton.setText("Edit");
+				}
+
+			}
+
+		});
+
 		// Add Checkbox Functionality
+		// Create Strikethrough Style
+		StyledDocument doc = taskText.getStyledDocument();
+		Style style = taskText.addStyle("StrikethroughStyle", null);
+
 		checkbox.addItemListener(new ItemListener() {
 			@Override
 			public void itemStateChanged(ItemEvent e) {
 
-				// Striked text when checkbox is checked
-				StyledDocument doc = taskTextPane.getStyledDocument();
-				Style style = taskTextPane.addStyle("StrikethroughStyle", null);
-
+				// Striked Text When Checkbox Is Checked
 				if (e.getStateChange() == ItemEvent.SELECTED) {
 					try {
 						StyleConstants.setStrikeThrough(style, true);
-						String text = taskTextPane.getText();
-						taskTextPane.setText("");
+						String text = taskText.getText();
+						taskText.setText("");
 						doc.insertString(doc.getLength(), text, style);
 
 					} catch (Exception ex) {
@@ -136,16 +201,18 @@ public class MainWindow {
 					}
 
 				}
+				// Disable Striked Text When Checkbox Is Unchecked
 				if (e.getStateChange() == ItemEvent.DESELECTED) {
+
 					try {
 						StyleConstants.setStrikeThrough(style, false);
-						String text = taskTextPane.getText();
-						taskTextPane.setText("");
+						String text = taskText.getText();
+						taskText.setText("");
 						doc.insertString(doc.getLength(), text, style);
-
 					} catch (Exception ex) {
 						ex.printStackTrace();
 					}
+
 				}
 
 			}
@@ -154,6 +221,8 @@ public class MainWindow {
 		// Customize Remove Button
 		removeButton.setFocusable(false);
 		removeButton.setPreferredSize(new Dimension(50, 30));
+
+		// Remove Button Functionality
 		removeButton.addActionListener(new ActionListener() {
 
 			@Override
@@ -167,93 +236,136 @@ public class MainWindow {
 
 		});
 
-		// Customize Edit Button
-		editButton.setFocusable(false);
-		editButton.setPreferredSize(new Dimension(70, 30));
-		
-		//Edit Button Functionality
-		editButton.addActionListener(new ActionListener() {
-		
-			@Override
-			public void actionPerformed(ActionEvent e) {
+		// Add Text From Parameter To Task Text
+		// This Is Useful When Loading Tasks From File
+		taskText.setText(task);
 
-				if (editButton.getText().equals("Edit")) {
-					taskTextPane.setEnabled(true);
-					editButton.setText("Done");
-				} else if (editButton.getText().equals("Done")) {
-					taskTextPane.setEnabled(false);
-					editButton.setText("Edit");
-				}
+		// Customize Task Text
+		taskText.setFont(new Font("Arial", Font.PLAIN, 25));
+		taskText.setPreferredSize(new Dimension(350, 30));
+		taskText.setDisabledTextColor(Color.gray);
+		taskText.setBorder(new LineBorder(Color.black, 1));
 
-			}
-
-		});
-		
-		
-		// Customize Task Text Pane
-		taskTextPane.setFont(new Font("Arial", Font.PLAIN, 25));
-		taskTextPane.setPreferredSize(new Dimension(350, 30));
-		taskTextPane.setDisabledTextColor(Color.gray);
-		taskTextPane.setBorder(new LineBorder(Color.black, 1));
-		Document taskDoc = taskTextPane.getDocument();
-
-		// Disable Checkbox If Task Is Empty
-		taskDoc.addDocumentListener(new DocumentListener() {
-			@Override
-			public void changedUpdate(DocumentEvent arg0) {
-			}
-
-			@Override
-			public void insertUpdate(DocumentEvent arg0) {
-				checkbox.setEnabled(true);
-				
-			}
-
-			@Override
-			public void removeUpdate(DocumentEvent arg0) {
-				if (taskTextPane.getText().isEmpty()) {
-					checkbox.setEnabled(false);
-					
-				}
-
-			}
-		});
-
-		// Add Elements
+		// Add Elements To Task Panel
 		taskPanel.add(checkbox);
-		taskPanel.add(taskTextPane);
+		taskPanel.add(taskText);
 		taskPanel.add(editButton);
 		taskPanel.add(removeButton);
+
+		// Checkbox State Handling (Strikethrough Text And Select Checkbox If State == 1
+		// Useful For Loading From File
+		if (state == STATE.CHECKED) {
+			checkbox.setSelected(true);
+			try {
+				StyleConstants.setStrikeThrough(style, true);
+				String text = taskText.getText();
+				taskText.setText("");
+				doc.insertString(doc.getLength(), text, style);
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		} else if (state == STATE.UNCHECKED) {
+			checkbox.setSelected(false);
+
+		}
 
 		return taskPanel;
 
 	}
-	
-	
+
+	// Saving Task Text Data To Text File
+	// Saving Task Checkbox State To Text File
 	private void saveTasks() {
-		
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter("tasks.txt"));
+
+			// Find Checkbox For Every Task in Tasks List
+			// Find Task Text For Every Task in Tasks List
+			// Write 0 Into Line To Mark Checkbox State As Unchecked
+			// Write 1 Into Line To Mark Checkbox State As Checked
+			for (JPanel panel : tasks) {
+				for (Component component : panel.getComponents()) {
+
+					if (component instanceof JCheckBox) {
+						JCheckBox checkbox = (JCheckBox) component;
+
+						if (checkbox.isSelected()) {
+							writer.write("1");
+
+						} else {
+							writer.write("0");
+						}
+
+					}
+
+					if (component instanceof JTextPane) {
+						JTextPane textPane = (JTextPane) component;
+
+						writer.write(textPane.getText());
+						writer.write("\n");
+
+					}
+
+				}
+			}
+
+			writer.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	// To Do:
-	// Tasks Speichern
-	// Um auf Text zuzugreifen, maybe eine Klasse machen für den Typ der Liste,
-	// welche den Text des JTextPanes des JPanels speichert
-	// wo die Tasks sind.
-	// Oder den text getten indem man durch die Liste iterated, bis man sie findet
-	// und dann das Element jeweils findet und
-	// Das in einer JTextPane variable speichert und so den text kriegt.
-	
-	//Hilfestellung:
-	//https://www.youtube.com/watch?v=ScUJx4aWRi0
-	/*
-	    for (JPanel panel : panelList) {
-            // Assuming each panel contains one JTextPane, get the first component
-            for (Component component : panel.getComponents()) {
-                if (component instanceof JTextPane) {
-                    JTextPane textPane = (JTextPane) component;
-                    System.out.println("Text in JTextPane: " + textPane.getText());
-                }
-            }
-	 */
+	private void loadTasks() {
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("tasks.txt"));
+
+			String line;
+			String textWithoutStateNumber;
+
+			// Read State Number And Set Checkbox State Accordingly
+			// Create Task Without State Number In The Text
+			while ((line = reader.readLine()) != null) {
+
+				if ((line.contains("0"))) {
+					textWithoutStateNumber = "";
+					char[] array = line.toCharArray();
+
+					for (int i = 0; i < array.length; i++) {
+						if (array[i] != '0') {
+							textWithoutStateNumber += array[i];
+						}
+					}
+
+					tasks.add(addTask(textWithoutStateNumber, STATE.UNCHECKED));
+					mainPanel.add(tasks.getLast());
+
+				} else if ((line.contains("1"))) {
+					textWithoutStateNumber = "";
+					char[] array = line.toCharArray();
+
+					for (int i = 0; i < array.length; i++) {
+						if (array[i] != '1') {
+							textWithoutStateNumber += array[i];
+						}
+
+					}
+
+					tasks.add(addTask(textWithoutStateNumber, STATE.CHECKED));
+					mainPanel.add(tasks.getLast());
+				}
+
+			}
+
+			reader.close();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
 
 }
